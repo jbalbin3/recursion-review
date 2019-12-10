45fc31019dae899365ac5d3d356cdd8443cf0d16
Kraig5 <KraigHanson@GMail.Com>
2019-12-09 19:19:10 -0700

    Completed stringifyJSON & fixed tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,28 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) { return 'null'; }
+  if (typeof obj === 'string') { return '"' + obj + '"'; }
+  if (typeof obj === 'number') { return obj.toString(); }
+  if (typeof obj === 'boolean') { return obj.toString(); }
+  if (typeof obj === 'undefined') { return undefined; }
+  if (typeof obj === 'function') { return undefined; }
+
+  if (typeof obj === 'object' && Array.isArray(obj)) {
+    return '[' + obj.map(e => stringifyJSON(e)) + ']';
+  }
+
+  if (typeof obj === 'object') {
+    return '{' + mapObj(obj, (k, v) => stringifyJSON(k) + ':' + stringifyJSON(v)) + '}';
+  }
+
+  let a = [];
+  var mapObj = function (o, f) {
+    for (k in o) {
+      if (typeof o[k] !== 'function' && typeof o[k] !== 'undefined') {
+        a.push(f(k, o[k]));
+      }
+    }
+    return a;
+  };
 };
